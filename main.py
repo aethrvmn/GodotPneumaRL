819bb524f830c6520bb5e0b301047bca59374e9c
Vasilis Valatsos <vasilvalat@gmail.com>
2024-05-22 09:28:47 +0200

    Added x86 executable

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from agent import main
 import args
 
 for policy_name, policy in policies.items():
-    main(policy_name=policy_name, policy=policy, parseargs=args.parse_args())
+    if policy_name in ["policy_big_tanh", "policy_big_optim_tanh"]:
+        print(policy_name)
+        main(policy_name=policy_name, policy=policy, parseargs=args.parse_args())
